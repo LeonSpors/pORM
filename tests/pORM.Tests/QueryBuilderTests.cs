c8189605a677cf46e058e05e89de6c4b9578ce80
Leon Spors <15744333+LeonSpors@users.noreply.github.com>
2026-08-24 23:01:33 +0200

    Fix Query Ordering and Paging
    Merge query ordering and paging fixes into develop.

--- tests/pORM.Tests/QueryBuilderTests.cs
+++ tests/pORM.Tests/QueryBuilderTests.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
 using System.Linq.Expressions;
 using NSubstitute;
 using NUnit.Framework;
 using pORM.Core.Interfaces;
+using pORM.Core.Models;
 using pORM.Mapping;
 using pORM.Tests.Models;
 
@@ -42,10 +44,53 @@ public class QueryBuilderTests
         Assert.Throws<ArgumentNullException>(() => query.OrderBy<int>(null!));
     }
 
-    private static IQuery<TestEntity> CreateQuery()
+    [Test]
+    public void Query_WithMultipleOrderings_BuildsAllOrderClauses()
+    {
+        ITableCache cache = Substitute.For<ITableCache>();
+        cache.GetItem(Arg.Any<PropertyInfo>())
+            .Returns(callInfo => new TableCacheItem(callInfo.Arg<PropertyInfo>()));
+        Query<TestEntity> query = CreateQuery(cache);
+
+        query.OrderBy(entity => entity.Name)
+            .ThenByDescending(entity => entity.Id);
+
+        string sql = GetSql(query);
+
+        Assert.That(sql, Does.Contain("ORDER BY Name ASC, Id DESC"));
+    }
+
+    [Test]
+    public void Query_WithSkipOnly_UsesUnboundedLimit()
+    {
+        Query<TestEntity> query = CreateQuery();
+
+        query.Skip(10);
+
+        string sql = GetSql(query);
+
+        Assert.That(sql, Does.Contain("LIMIT 18446744073709551615 OFFSET 10"));
+    }
+
+    private static Query<TestEntity> CreateQuery()
     {
         IDatabaseConnectionFactory factory = Substitute.For<IDatabaseConnectionFactory>();
         ITableCache cache = Substitute.For<ITableCache>();
         return new Query<TestEntity>(factory, cache, "sample_table");
+    }
+
+    private static Query<TestEntity> CreateQuery(ITableCache cache)
+    {
+        IDatabaseConnectionFactory factory = Substitute.For<IDatabaseConnectionFactory>();
+        return new Query<TestEntity>(factory, cache, "sample_table");
+    }
+
+    private static string GetSql(Query<TestEntity> query)
+    {
+        MethodInfo buildQuery = typeof(Query<TestEntity>).GetMethod(
+            "BuildQuery",
+            BindingFlags.Instance | BindingFlags.NonPublic)!;
+        object queryParts = buildQuery.Invoke(query, new object[] { false })!;
+        return (string)queryParts.GetType().GetProperty("Sql")!.GetValue(queryParts)!;
     }
 }
